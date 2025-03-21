75d954127f7a1e4ef8cad82c0e5c924faf2f95ea
Anastasia <a.lilova@sertink.ru>
2025-03-21 23:01:12 +0300

    review 2

--- scripts/index.js
+++ scripts/index.js
@@ -4,31 +4,34 @@ const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 
-const placeCards = document.querySelector(".places__list");
+const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
-function createCard(initialCards, closeCard) {
+function createCard(initialCards, deleteCard) {
   const card = cardTemplate.querySelector(".card").cloneNode(true); //клонируем со всем содержимым
   const cardImage = card.querySelector(".card__image");
   const cardTitle = card.querySelector(".card__title");
   const buttonDelete = card.querySelector(".card__delete-button");
   cardImage.src = initialCards.link;
+  cardImage.alt = initialCards.name;
   cardTitle.textContent = initialCards.name;
   buttonDelete.addEventListener("click", function () {
-    closeCard(buttonDelete);
+    deleteCard(buttonDelete);
   });
   return card;
 }
+
 // @todo: Функция удаления карточки
 
-function closeCard(buttonDelete) {
+function deleteCard(buttonDelete) {
   const cardListItem = buttonDelete.closest(".card");
   cardListItem.remove();
 }
+
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((card) => {
-  const newCard = createCard(card, closeCard);
-  placeCards.append(newCard); //новые добавляются в конец
+  const newCard = createCard(card, deleteCard);
+  cardsContainer.append(newCard); //новые добавляются в конец
 });
