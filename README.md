52348d935e1eaaa53dc81e57410ff880e1661ecd
Anastasia <a.lilova@sertink.ru>
2025-03-21 15:18:33 +0300

    review 1

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Проектная работа Mesto
-
+https://github.com/Anastasia9044?tab=repositories
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
+
+const placeCards = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+function createCard(initialCards, closeCard) {
+  const card = cardTemplate.querySelector(".card").cloneNode(true); //клонируем со всем содержимым
+  const cardImage = card.querySelector(".card__image");
+  const cardTitle = card.querySelector(".card__title");
+  const buttonDelete = card.querySelector(".card__delete-button");
+  cardImage.src = initialCards.link;
+  cardTitle.textContent = initialCards.name;
+  buttonDelete.addEventListener("click", function () {
+    closeCard(buttonDelete);
+  });
+  return card;
+}
 // @todo: Функция удаления карточки
 
+function closeCard(buttonDelete) {
+  const cardListItem = buttonDelete.closest(".card");
+  cardListItem.remove();
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((card) => {
+  const newCard = createCard(card, closeCard);
+  placeCards.append(newCard); //новые добавляются в конец
+});
